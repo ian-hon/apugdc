7ceeb3d16a9122dcb1f12463035cc6ac66283368
ajian_nedo <asianerd28@gmail.com>
2024-10-21 10:41:54 +0800

    Implemented dynamic info system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import styles from "./page.module.css";
 import headerBackground from "./assets/space.jpg";
 import Image from "next/image";
 import CountUp from "react-countup";
-import Footer from "./footer";
+import Footer from "./components/footer";
 import Link from "next/link";
+import { useState } from "react";
 
-const data = [
+const counterData = [
     {
         number: 4,
         description: 'Past & upcoming events',
@@ -30,7 +31,60 @@ const data = [
     },
 ]
 
+const infoSection = [
+    [
+        'Events',
+        <div id={styles.info}>
+            <h2>
+                Past & Upcoming Events
+            </h2>
+        </div>
+    ],
+    [
+        'R&D',
+        <div id={styles.rnd}>
+            <h2>
+                Join our R&D Team to Shape Your Future in The Gaming Industry!
+            </h2>
+            <h3>
+                What are you waiting for? Join our R & D Department & Collaborate with Like-Minded Peers to Work on Various Projects & Do Research.
+            </h3>
+            <h3>
+                If you are not a member, join the APU Game Development Club now! 🎮‼️
+            </h3>
+            <Link href="https://forms.gle/xRNK1CXzDuJRJgug8" target="_blank">
+                <h2>
+                    Join now
+                </h2>
+            </Link>
+            <h3>
+                And plus, it would look good on your resumé too.😏
+            </h3>
+        </div>
+    ],
+    [
+        'Hall of Fame',
+        <div id={styles.info}>
+            <h2>
+                Hall of Fame
+            </h2>
+        </div>
+    ],
+    [
+        'About APUGDC',
+        <div id={styles.about}>
+            <h2>Welcome to the <b>APU Game Development Club!</b></h2>
+            <h3>We're here to promote the development of video games, and we host <b>workshops, game jams</b> and <b>many more</b> to encourage everyone to learn about <u>game design and development</u>.</h3>
+            <h3>We are open to students of <u>all levels of experience.</u></h3>
+            <Link href=""><h2>Join us and find out more!</h2></Link>
+        </div>
+    ],
+]
+
+
 export default function Home() {
+    const [section, selectSection] = useState(0);
+
     return (
         <div id={styles.page}>
             <div id={styles.titleHeader}>
@@ -41,17 +95,19 @@ export default function Home() {
                     <h2>
                         Empowering the next generation of game developers
                     </h2>
+                    <Link title="it's free!" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfQH_7SUjGGpFqssLeJK67P60P8Yei32xYar-dVQ-3joDpR0g/viewform?usp=pp_url">
+                        Join APUGDC!
+                    </Link>
                 </div>
                 <Image id={styles.headerBackground} src={headerBackground} alt=""/>
             </div>
-            <div id={styles.infoContainer}>
+            <div id={styles.counterContainer}>
                 {
-                    data.map((e) => 
+                    counterData.map((e) => 
                         (
                             <div className={styles.info}>
                                 <div>
                                     <CountUp end={e.number} duration={5} />
-                                    {/* <h2>{e.number}</h2> */}
                                     <div/>
                                 </div>
                                 <h3>{e.description}</h3>
@@ -60,37 +116,26 @@ export default function Home() {
                     )
                 }
             </div>
-            <div className={styles.hr}/>
-            <div id={styles.linkContainer}>
-                <a>
-                    <h3>
-                        R&D Program
-                    </h3>
-                </a>
-                <a>
-                    <h2>
-                        Become an APUGDC member!
-                    </h2>
-                </a>
-                <a>
-                    <h3>
-                        What is APUGDC?
-                    </h3>
-                </a>
-                <Link href="/hall-of-fame">
-                    <h3>
-                        Hall of Fame
-                    </h3>
-                </Link>
-            </div>
-            <div className={styles.hr}/>
-            <div id={styles.eventContainer}>
-                <h1>Events</h1>
-                <div>
-                    <h2>
-                        insta embed here
-                    </h2>
+            {/* <div className={styles.hr}/> */}
+            <div id={styles.infoContainer}>
+                {/* module.css call necessary? */}
+                {/* TODO: regular string call might be good enough */}
+                <div id={styles.infoSelection}>
+                    {
+                        infoSection.map(([k, _], i) => (
+                            <div
+                                className={ i == section ? styles.selected : "" }
+                                onClick={() => { selectSection(i); }}
+                            >
+                                <h2>{k}</h2>
+                                <div className={styles.hr} />
+                            </div>
+                        ))
+                    }
                 </div>
+                {
+                    infoSection[section][1]
+                }
             </div>
             <Footer/>
         </div>
