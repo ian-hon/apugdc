a9fc52004aec7797eb6f51a5065cf6bf86b9f1ba
ajian_nedo <asianerd28@gmail.com>
2024-10-25 10:40:08 +0800

    What? section for R&D page

--- src/app/rnd/page.tsx
+++ src/app/rnd/page.tsx
@@ -27,7 +27,21 @@ const infoContent = [
             <h1>
                 Why join R&D?
             </h1>
-            <div>
+            <div id={styles.why}>
+                <span>
+                    <h2>
+                        Ever wanted to work on a <u>meaningful</u> project?
+                    </h2>
+                    <h2>
+                        Don't know how to where to get started?
+                    </h2>
+                </span>
+                <h2>
+                    Join & collaborate with other like-minded people on their projects!
+                </h2>
+                <JoinButton contain size={'0.8em'}/>
+            </div>
+            {/* <div id={styles.why}>
                 <span>
                     <h2>
                         Ever wanted to work on a <u>meaningful</u> project?
@@ -43,13 +57,58 @@ const infoContent = [
                     More details in the next sections, but if you're already interested then why not just join us already?
                 </h2>
                 <JoinButton contain size={'0.8em'}/>
-            </div>
+            </div> */}
         </div>
     ],
     [
         'What?',
         <div id={styles.infoContainer}>
-            what
+            {/* 
+                Research and Development (R&D) is a special group under APUGDC, open to all members.
+
+                The R&D team focuses on researching and innovating game development, from programming and design to sound engineering.
+
+                We want to help YOU kick start your journey in becoming a better game developer.
+
+                Showcase your talents and passion with other like-minded people, regardless of your skill, background or course.
+
+                Note : R&D members are not APUGDC committee, and committees will be treated the same as any other member.
+
+                We do not control, supervise, moderate or own your projects. All rights are to your team. We serve only to motivate, guide and help you in your research.
+
+             */}
+            <h1>
+                What is R&D?
+            </h1>
+            <div id={styles.what}>
+                <span>
+                    <h2>
+                        <b>Research and Development (R&D)</b> group is a special group under APUGDC that is open to <i>all members.</i>
+                    </h2>
+                    <h2>
+                        The R&D team focuses on <u>researching and innovating</u> game development, from <b>programming</b> and <b>design</b> to <b>sound engineering.</b>
+                    </h2> 
+                </span>
+                <span>
+                    <h2>
+                        We want to help <u>YOU</u> kick start your journey in becoming a <b>better game developer.</b>
+                    </h2>
+                    <h2>
+                        Showcase your talents and passion with other like-minded people, <i>regardless of your skill, background or course.</i>
+                    </h2>
+                </span>
+                <div id={styles.note}>
+                    <h2>Note : </h2>
+                    <ul>
+                        <li>
+                            R&D members are not APUGDC committee, and committees will be treated the same as any other member.
+                        </li>
+                        <li>
+                            We do not control, supervise, moderate or own your projects. All rights are to your team. We serve only to motivate, guide and help you in your research.
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     ],
     [
