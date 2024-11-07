47b4da0de6e98f8a5f231a3cdb5e149c67168211
ajian_nedo <asianerd28@gmail.com>
2024-11-07 09:49:58 +0800

    Testing: Changed font load order

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,12 @@ const nasaliza = localFont({
     variable: "--nasaliza-font",
     weight: "100 900",
 });
-const nunitosans = Nunito_Sans({ subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'vietnamese'], variable: '--nunitosans-font' });
 // const robotomono = Roboto_Mono({ subsets: ['latin', 'latin-ext'], variable: '--robotomono-font' });
 const robotomono = Roboto_Mono({ subsets: ['cyrillic', 'cyrillic-ext', 'greek', 'latin', 'latin-ext', 'vietnamese'], weight: ['100', '200', '300', '400', '500', '600', '700'], variable: '--robotomono-font' });
 const firacode = Fira_Code({ subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'greek', 'greek-ext'], variable: '--firacode-font' });
 const roboto = Roboto({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'greek', 'greek-ext', 'vietnamese'], variable: '--roboto-font' });
 const open_sans = Open_Sans({ weight: [ '300', '400', '500', '600', '700', '800' ], subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'greek', 'greek-ext', 'hebrew', 'math', 'symbols', 'vietnamese'], variable: '--opensans-font' });
+const nunitosans = Nunito_Sans({ subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'vietnamese'], variable: '--nunitosans-font' });
 
 export const metadata: Metadata = {
     title: "APUGDC",
