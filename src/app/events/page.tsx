d773f77d19fd00025eae0453908ead417e43bd9c
ajian_nedo <asianerd28@gmail.com>
2024-11-12 23:30:45 +0800

    Dynamic asset loading + change to gradient background

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,16 +1,64 @@
 "use client";
 
+import Image from "next/image";
+
 import Footer from "../components/footer";
 import { ReturnButton } from "../components/returnButton";
 
 import '../globals.css';
-
 import styles from './events.module.css';
 
-import image_0 from '../assets/events/multiplayer_talk/0.png';
-import Image from "next/image";
+const eventData = [
+    {
+        assetDirectory: 'multiplayer_talk',
+        title: 'Multiplayer Talk',
+        host: 'Jon Tang',
+        time: 1729728000000,
+        location: 'APU Campus'
+    },
+    {
+        assetDirectory: 'multiplayer_talk',
+        title: 'Multiplayer Talk',
+        host: 'Jon Tang',
+        time: 1729728000000,
+        location: 'APU Campus'
+    },
+    {
+        assetDirectory: 'multiplayer_talk',
+        title: 'Multiplayer Talk',
+        host: 'Jon Tang',
+        time: 1729728000000,
+        location: 'APU Campus'
+    },
+]
 
 export default function Events() : React.JSX.Element {
+    let thumbnail = eventData[0];
+    let collection = eventData.slice(1);
+
+    function TileElement({t}: { t:any }) {
+        let time = new Date(t.time);
+
+        return (
+            <div className={styles.tile} key={t.assetDirectory}>
+                <div id={styles.info}>
+                    <div id={styles.description}>
+                        <h2>
+                            {t.title}
+                        </h2>
+                        <h3>
+                            by {t.host}  •  {time.getDate()}/{time.getMonth()}/{time.getFullYear()}  •  {t.location}
+                        </h3>
+                    </div>
+                    <div id={styles.gradient}/>
+                </div>
+                <div id={styles.headerImage}>
+                    <Image src={`/events/${t.assetDirectory}/thumbnail.png`} width={1000} height={1000} alt=''></Image>
+                </div>
+            </div>
+        )
+    };
+
     return (
         <div id={styles.events}>
             <ReturnButton/>
@@ -23,21 +71,13 @@ export default function Events() : React.JSX.Element {
                 </h3>
             </div>
             <div id={styles.content}>
-                <div className={styles.tile}>
-                    <div id={styles.info}>
-                        <div id={styles.description}>
-                            <h2>
-                                Multiplayer Talk
-                            </h2>
-                            <h3>
-                                by Jon Tang • 24/10/2024
-                            </h3>
-                        </div>
-                        <div id={styles.gradient}/>
-                    </div>
-                    <div id={styles.headerImage}>
-                        <Image src={image_0} alt=''></Image>
-                    </div>
+                <TileElement t={thumbnail} />
+                <div id={styles.collection}>
+                    {
+                        collection.map((t) => (
+                            <TileElement t={t}/>
+                        ))
+                    }
                 </div>
             </div>
             <Footer/>
