4a0720c03feeb32a698285c1172c0c376e5c2b7b
ajian_nedo <asianerd28@gmail.com>
2024-11-12 12:35:24 +0800

    WIP Events page concept

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,8 +1,45 @@
+"use client";
+
 import Footer from "../components/footer";
+import { ReturnButton } from "../components/returnButton";
+
+import '../globals.css';
+
+import styles from './events.module.css';
+
+import image_0 from '../assets/events/multiplayer_talk/0.png';
+import Image from "next/image";
 
 export default function Events() : React.JSX.Element {
     return (
-        <div>
+        <div id={styles.events}>
+            <ReturnButton/>
+            <div id={styles.header}>
+                <h2>
+                    Events
+                </h2>
+                <h3>
+                    our past events
+                </h3>
+            </div>
+            <div id={styles.content}>
+                <div className={styles.tile}>
+                    <div id={styles.info}>
+                        <div id={styles.description}>
+                            <h2>
+                                Multiplayer Talk
+                            </h2>
+                            <h3>
+                                by Jon Tang • 24/10/2024
+                            </h3>
+                        </div>
+                        <div id={styles.gradient}/>
+                    </div>
+                    <div id={styles.headerImage}>
+                        <Image src={image_0} alt=''></Image>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </div>
     );
