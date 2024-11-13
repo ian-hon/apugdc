70a0a522e078150fda5d0dc4094f29731eae8bce
ajian_nedo <asianerd28@gmail.com>
2024-11-13 14:05:17 +0800

    Made submission form into embed

--- src/app/submission/page.tsx
+++ src/app/submission/page.tsx
@@ -1,5 +1,14 @@
 import { redirect } from "next/navigation";
+import styles from './submission.module.css';
+import { ReturnButton } from "../components/returnButton";
+import Footer from "../components/footer";
 
 export default function Submission() {
-    redirect('https://forms.gle/s4pp7u75n6B7ngsw7');
+    return (
+        <div id={styles.submission}>
+            <ReturnButton/>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSffjMjFgQxk44eMuF9z2cNeN0orSi9Kb1lVXp0t2vO8EKU8fg/viewform?embedded=true" width="640" height="2100" frameBorder={0}>Loading…</iframe>
+            <Footer/>
+        </div>
+    )
 }
