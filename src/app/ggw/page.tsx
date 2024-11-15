e227899e8bc4a163e820eeaee966819c39102b68
ajian_nedo <asianerd28@gmail.com>
2024-11-16 01:39:53 +0800

    Empty GGW page

--- src/app/ggw/page.tsx
+++ src/app/ggw/page.tsx
@@ -1,11 +1,16 @@
 import styles from './ggw.module.css';
 import global_styles from '../page.module.css';
 import { ReturnButton } from '../components/returnButton';
+import Footer from '../components/footer';
 
 export default function GGW() {
     return (
-        <div>
-            
+        <div id={styles.ggw}>
+            <ReturnButton/>
+            <div>
+                
+            </div>
+            <Footer/>
         </div>
     )
 }
