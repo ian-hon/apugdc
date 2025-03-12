ce349b7e1c117096070458f4f07c29d5678d84ed
ajian_nedo <asianerd28@gmail.com>
2025-03-12 22:22:46 +0800

    Changing between committee and member views

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -9,25 +9,80 @@ import { BACKEND_ADDRESS } from '../constants';
 
 function LeaderboardElement({ e, i }: { e: any, i: any }) {
     return <div className={styles.leaderboardElement}>
-        <h3>
-            {e.user.name}
-        </h3>
         <h4>
             {i}
+        </h4>
+        <h3>
+            {e.name}
+        </h3>
+        <h4>
+            {e.points}
         </h4>
     </div>
 }
 
 export default function Leaderboard() {
     const [board, changeBoard] = useState<any[]>([]);
+    const [allEntries, changeAllEntries] = useState<any[]>([]);
+
+    const [committeeView, changeCommitteeView] = useState<boolean>(false);
 
     useEffect(() => {
-        fetch(`${BACKEND_ADDRESS}/leaderboard/fetch`)
-        .then(r => r.json())
-        .then(board => {
-            changeBoard(board.sort((a: any, b: any) => b.points - a.points));
-        })
+        let result = [
+            {
+                name:'lorem',
+                points:100,
+                isCommittee:false
+            },
+            {
+                name:'ipsum',
+                points:200,
+                isCommittee:false
+            },
+            {
+                name:'dolor',
+                points:300,
+                isCommittee:true
+            },
+            {
+                name:'sit',
+                points:400,
+                isCommittee:false
+            },
+            {
+                name:'amet',
+                points:120,
+                isCommittee:false
+            },
+            {
+                name:'consectetur',
+                points:40,
+                isCommittee:true
+            },
+            {
+                name:'adispiscing',
+                points:60,
+                isCommittee:false
+            },
+            {
+                name:'elit',
+                points:90,
+                isCommittee:true
+            },
+        ]
+        result = result.sort((a: any, b: any) => b.points - a.points);
+        changeAllEntries(result);
+        filterBoard(result);
+        // fetch(`${BACKEND_ADDRESS}/leaderboard/fetch`)
+        // .then(r => r.json())
+        // .then(board => {
+        //     changeBoard(board.sort((a: any, b: any) => b.points - a.points));
+        // })
     }, []);
+
+    function filterBoard(all: any[]) {
+        changeBoard(all.filter((x) => x.isCommittee == committeeView));
+    }
 
     return <div id={styles.page}>
         <ReturnButton/>
@@ -37,6 +92,14 @@ export default function Leaderboard() {
         <h1 id={styles.title}>
             APUGDC 2025 Battlepass
         </h1>
+        <div id={styles.tabSelection}>
+            <h2 onClick={() => { changeCommitteeView(false); filterBoard(allEntries); }} id={styles.tabItem} aria-label={committeeView ? '' : 'active'}>
+                members
+            </h2>
+            <h2 onClick={() => { changeCommitteeView(true); filterBoard(allEntries); }} id={styles.tabItem} aria-label={committeeView ? 'active' : ''}>
+                committees
+            </h2>
+        </div>
         <div id={styles.podium}>
             {
                 (board.length >= 3) &&
@@ -47,7 +110,7 @@ export default function Leaderboard() {
                 ].map((e: any, index: any) => 
                     <div className={styles.stand} key={index}>
                         <h3>
-                            {e[0].user.name}
+                            {e[0].name}
                         </h3>
                         <div id={styles.fill} style={{
                             backgroundColor:e[2].toString(),
